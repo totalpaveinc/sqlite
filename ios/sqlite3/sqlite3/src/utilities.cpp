56f156c49264878aa30c73d1a32820961514e749
Tyler Breau <tylerbreau@hotmail.com>
2022-12-20 14:03:11 -0400

    Implemented system to build .framework for the .xcframeworks. This is done because iOS does not support .dylib, only Mac supports .dylib. Renamed iOS project and related things from sqlite to sqlite3. This is primarily so that the framework and xcframework name matches with the third-party library.

---
+++ ios/sqlite3/sqlite3/src/utilities.cpp
@@ -0,0 +1,33 @@
+/*
+    Copyright 2022 Total Pave Inc.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this
+    software and associated documentation files (the "Software"), to deal in the Software
+    without restriction, including without limitation the rights to use, copy, modify,
+    merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies
+    or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+    PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <tp/sqlite/utilities.h>
+#include <sqlite3.h>
+
+namespace TP { namespace sqlite {
+    // https://www.sqlite.org/c3ref/bind_blob.html
+    // When the same named SQL parameter is used more than once, second and subsequent occurrences have the same index as the first occurrence.
+    int lookupVariableIndex(sqlite3_stmt* statement, const char* variable) {
+        std::string strVar = variable;
+        std::string boundVarName = ":" + strVar;
+        return sqlite3_bind_parameter_index(statement, boundVarName.c_str());
+    }
+}}
